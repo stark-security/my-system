a7aa23baf392aee0b09bb5673581188c06c8122b
Anderson B Silva/ <32654298+oandersonbsilva@users.noreply.github.com>
2019-03-31 21:15:00 -0300

    Update my-system.sh

--- my-system.sh
+++ my-system.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#Acesso ROOT. 
+
+[ $UID -ne '0' ] && { echo "Necessário ter Acesso ROOT."; exit 1 ;}
 
 okegreen='\033[92m'
 
@@ -11,7 +15,7 @@ echo -e $okegreen "
 echo "Autor: Anderson.b.Silva"
 echo ""
 
-select i in Processador Memória Unidades/montadas Drivers/vídeo Placa/Som Resolução/Telas Hostname Data/Instalação Rede scanner Espaço/Disco Sistema Logs sair
+select i in Processador Memória Unidades/montadas Drivers/vídeo Placa/Som Resolução/Telas Hostname Data/Instalação Rede Espaço/Disco Sistema Logs sair
 do
 
    case "$i" in
@@ -21,8 +25,8 @@ do
          head -n 29 /proc/cpuinfo
          ;;
 
-    Memória)
-         echo "Memória RAM e SWAP (em MB)"
+      Memória)
+        echo "Memória RAM e SWAP (em MB)"
         free -m
          ;;
 
@@ -31,83 +35,71 @@ do
          df -hT
          ;;
 
-       Drivers/vídeo)
-         echo "Driver utilizado pelo X para sua placa de vídeo"
-         grep -B1 "/drivers" /var/log/Xorg.0.log | head -n 2
+      Drivers/vídeo)
+        echo "Driver utilizado pelo X para sua placa de vídeo"
+        grep -B1 "/drivers" /var/log/Xorg.0.log | head -n 2
          ;;
 
-         Placa/Som)
-           echo "Placa de som "
-           aplay -l
+      Placa/Som)
+        echo "Placa de som "
+        aplay -l
+         ;;
+
+      Resolução/Telas)
+        echo "Resolução de telas suportadas"
+        xrandr
+         ;;
+
+      Hostname)
+        echo "Nome do computador (Hostname)"
+        hostname
+         ;;
+
+      Data/Instalação)
+        echo "Data da instalação"
+        ls -lct /etc | tail -1 | awk '{print $6, $7, $8}'
+         ;;
+
+      Mídias)
+        echo "Data da instalação"
+        ls -lct /etc | tail -1 | awk '{print $6, $7, $8}'
+         ;;
+
+      Rede)
+        echo "Rede"
+        echo -e "[+] Verificando conexao com a Internet..."
+        sleep 0.15
+        echo -e " um momento!\n"
+        ping -c 1 google.com &> /dev/null
+          if [ $? -gt 0 ]; then
+          ping -c 1 uol.com.br &> /dev/null
+        	if [ $? -gt 0 ]; then
+          echo "Verifique sua conexao..."
+          echo ""
+           exit 1
+          	fi
+             fi
+          echo -e "[+] Internet OK!\n"
+          sleep 0.15
+
            ;;
 
-           Resolução/Telas)
-             echo "Resolução de telas suportadas"
-             xrandr
-             ;;
+       Espaço/Disco)
+       echo "Verificação de espaço em disco"
+       df -h
+          ;;
 
-             Hostname)
-               echo "Nome do computador (Hostname)"
-               hostname
-               ;;
+      Sistema)
+       echo "Seu Sistema"
+       cat /etc/os-release
+          ;;
 
-               Data/Instalação)
-                 echo "Data da instalação"
-                 ls -lct /etc | tail -1 | awk '{print $6, $7, $8}'
-                 ;;
+       Logs)
+       echo "Verificação de Logs"
+       tail -400 /var/log/messages
+          ;;
 
-                 Mídias)
-                   echo "Data da instalação"
-                   ls -lct /etc | tail -1 | awk '{print $6, $7, $8}'
-                   ;;
-
-                  Rede)
-                     echo "Rede"
-                     echo -e "[+] Verificando conexao com a Internet..."
-                     sleep 0.15
-                     echo -e " um momento!\n"
-                     ping -c 1 google.com &> /dev/null
-                     if [ $? -gt 0 ]; then
-                     	ping -c 1 uol.com.br &> /dev/null
-                     	if [ $? -gt 0 ]; then
-                     		echo "Verifique sua conexao..."
-                     		echo ""
-                     		exit 1
-                     	fi
-                     fi
-                     echo -e "[+] Internet OK!\n"
-                     sleep 0.15
-
-                     ;;
-
-                     scanner)
-                        echo ""
-   echo -e "\tDigite o ip: \n"
-   read ip
-   echo -e "\tColoque o primeiro numero de: \n"
-   read porta
-   echo -e "\tColoque o segundo numero $porta ha:\n"
-   read porta2
-   echo -e "\tcontinuar [enter]\n"
-   read
-   echo -e `netcat -vv $ip $porta\-\$porta2`
-                        ;;
-
-                         Espaço/Disco)
-                           echo "Verificação de espaço em disco"
-          df -h
-                           ;;
-
-                           Sistema)
-                              echo "Seu Sistema"
-                              cat /etc/os-release
-                              ;;
-
-                              Logs)
-                                 echo "Verificação de Logs"
-                                 tail -400 /var/log/messages
-                                 ;;
-      sair)
+       sair)
          echo "See you later!"
          break
          ;;
